7b621048e891256b1e75624f1e80d8cbe91d32ce
Steve <stephen.thomas@dovetail-genomics.com>
2020-12-27 15:02:23 -0800

    Added formiulate improt

--- vue-app/src/main.js
+++ vue-app/src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
+import VueFormulate from '@braid/vue-formulate'
 
 Vue.config.productionTip = false
+Vue.use(VueFormulate)
 
 new Vue({
   vuetify,
