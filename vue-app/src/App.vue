56d48152994228fb1900be380230d297c163df55
Breanna Baltaxe-Admony <bbaltaxe@ucsc.edu>
2020-12-27 14:53:19 -0800

    form bugs

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -8,19 +8,26 @@
       <v-spacer></v-spacer>
 
     </v-app-bar>
-
+    
     <v-main>
         <h1>Test</h1>
+
+        <Requests/>
+
     </v-main>
   </v-app>
 </template>
 
+
 <script>
+
+import Requests from './components/Requests.vue'
 
 export default {
   name: 'App',
 
   components: {
+    Requests
   },
 
   data: () => ({
