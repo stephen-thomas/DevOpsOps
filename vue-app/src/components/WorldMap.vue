7e08de30cb262c972439df06a6eed20f468e6558
Steve <stephen.thomas@dovetail-genomics.com>
2020-12-30 10:26:07 -0800

    Finished coding crude location from IP info

--- vue-app/src/components/WorldMap.vue
+++ vue-app/src/components/WorldMap.vue
@@ -1,37 +1,86 @@
 <template>
-  <svg id="my_dataviz" width="630" height="1000"></svg>
+  
+    <v-main class="grey lighten-3">
+      <v-container>
+        <v-row>
+          <v-col
+            cols="12"
+            sm="2"
+          >
+            <v-sheet
+              rounded="lg"
+              min-height="228"
+            >
+              <h1>Location</h1>
+              <p>{{city}}</p>
+              <p>{{country}}</p>
+              <p>{{region}}</p>
+              <p>{{ip}}</p>
+            </v-sheet>
+          </v-col>
+
+          <v-col
+            cols="12"
+            sm="8"
+          >
+            <v-sheet
+              id="parent"
+              min-height="70vh"
+              rounded="lg"
+            >
+              <svg height="100%" width="100%" id="my_dataviz"></svg>
+            </v-sheet>
+          </v-col>
+
+          <v-col
+            cols="12"
+            sm="2"
+          >
+            <v-sheet
+              rounded="lg"
+              min-height="268"
+            >
+              <!--  -->
+            </v-sheet>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
 </template>
 
 <script>
 import * as d3 from 'd3v4';
+import axios from 'axios';
 
 export default {
+  name: "WorldMap",
   data() {
     return {
-      svg: null,
-      width: null,
-      height: null,
-      projection: null,
-    };
+      location: null,
+      errorStr: null,
+      city: null,
+      country: null,
+      ip: null,
+      hostname: null,
+      timezone: null,
+      region: null,
+    }
   },
+
   methods: {
-    renderMap(error, dataGeo, data){
-        var projection = this.projection;
-        var svg = this.svg;
-        var width = this.width;
-        var height = this.height;
+    renderMap(error, dataGeo){
+        // The svg
+        var svg = d3.select("#my_dataviz")
+        svg.attr("preserveAspectRatio", "xMinYMin meet")
+            .attr("viewBox", "0 0 960 500");
+        
+        var width = 766;
+        var height = 662.188;
 
-        // Create a color scale
-        var allContinent = d3.map(data, function(d){return(d.homecontinent)}).keys()
-        var color = d3.scaleOrdinal()
-          .domain(allContinent)
-          .range(d3.scaleOrdinal(d3.schemeCategory10));
-
-        // Add a scale for bubble size
-        var valueExtent = d3.extent(data, function(d) { return +d.n; })
-        var size = d3.scaleSqrt()
-          .domain(valueExtent)  // What's in the data
-          .range([ 1, 50])  // Size in pixel
+        // Map and projection
+        var projection = d3.geoMercator()
+            .scale(100)
+            .translate([ width / 2, height / 2 ])
 
         // Draw the map
         svg.append("g")
@@ -44,95 +93,26 @@ export default {
                   .projection(projection)
               )
             .style("stroke", "none")
-            .style("opacity", .3)
+            .style("opacity", 1)
 
-        // Add circles:
-        svg
-          .selectAll("myCircles")
-          .data(data.sort(function(a,b) { return +b.n - +a.n }).filter(function(d,i){ return i<1000 }))
-          .enter()
-          .append("circle")
-            .attr("cx", function(d){ return projection([+d.homelon, +d.homelat])[0] })
-            .attr("cy", function(d){ return projection([+d.homelon, +d.homelat])[1] })
-            .attr("r", function(d){ return size(+d.n) })
-            .style("fill", function(d){ return color(d.homecontinent) })
-            .attr("stroke", function(d){ if(d.n>2000){return "black"}else{return "none"}  })
-            .attr("stroke-width", 1)
-            .attr("fill-opacity", .4)
-
-        // Add title and explanation
-        svg
-          .append("text")
-            .attr("text-anchor", "end")
-            .style("fill", "black")
-            .attr("x", width - 10)
-            .attr("y", height - 30)
-            .attr("width", 90)
-            .html("WHERE SURFERS LIVE")
-            .style("font-size", 14)
-
-
-        // --------------- //
-        // ADD LEGEND //
-        // --------------- //
-
-        // Add legend: circles
-        var valuesToShow = [100,4000,15000]
-        var xCircle = 40
-        var xLabel = 90
-        svg
-          .selectAll("legend")
-          .data(valuesToShow)
-          .enter()
-          .append("circle")
-            .attr("cx", xCircle)
-            .attr("cy", function(d){ return height - size(d) } )
-            .attr("r", function(d){ return size(d) })
-            .style("fill", "none")
-            .attr("stroke", "black")
-
-        // Add legend: segments
-        svg
-          .selectAll("legend")
-          .data(valuesToShow)
-          .enter()
-          .append("line")
-            .attr('x1', function(d){ return xCircle + size(d) } )
-            .attr('x2', xLabel)
-            .attr('y1', function(d){ return height - size(d) } )
-            .attr('y2', function(d){ return height - size(d) } )
-            .attr('stroke', 'black')
-            .style('stroke-dasharray', ('2,2'))
-
-        // Add legend: labels
-        svg
-          .selectAll("legend")
-          .data(valuesToShow)
-          .enter()
-          .append("text")
-            .attr('x', xLabel)
-            .attr('y', function(d){ return height - size(d) } )
-            .text( function(d){ return d } )
-            .style("font-size", 10)
-            .attr('alignment-baseline', 'middle')
           }
   },
 
+  // Retrieve Users Location At Startup to Populate Heatmap
+  created(){
+    const url = 'http://ipinfo.io/json?token=7c783b82a63603';
+    axios.get(url).then(response => {
+      this.location = response.data;
+      this.city = this.location.city;
+      this.country = this.location.country;
+      this.ip = this.location.ip;
+      this.hostname = this.location.hostname;
+      this.timezone = this.location.timezone;
+      this.region = this.location.region;
+    })
+
+  },
   mounted(){
-    // The svg
-    var svg = d3.select("#my_dataviz"),
-        width = +svg.attr("width"),
-        height = +svg.attr("height");
-
-    // Map and projection
-    var projection = d3.geoMercator()
-        .center([0,20])                // GPS of location to zoom on
-        .scale(99)                       // This is like the zoom
-        .translate([ width / 3, height / 3 ])
-
-    this.svg = svg;
-    this.projection = projection;
-
     d3.queue()
       .defer(d3.json, "https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson")  // World shape
       .defer(d3.csv, "https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/data_gpsLocSurfer.csv") // Position of circles
